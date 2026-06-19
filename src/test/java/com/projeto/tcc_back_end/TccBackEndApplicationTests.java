4a111a5c82a2378804d54beda807415b7751e8c5
lcn 777 <scherrer.lincoln09@gmail.com>
2026-06-19 14:05:27 -0300

    inicialização do tcc hub inteligente

---
+++ src/test/java/com/projeto/tcc_back_end/TccBackEndApplicationTests.java
@@ -0,0 +1,13 @@
+package com.projeto.tcc_back_end;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TccBackEndApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
